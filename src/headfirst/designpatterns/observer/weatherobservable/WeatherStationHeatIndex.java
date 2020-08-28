36d6546b25cd10c4cad3213fc2373ddb2f50c0de
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2020-08-27 20:07:24 -0400

    asd
    fasdf

--- src/headfirst/designpatterns/observer/weatherobservable/WeatherStationHeatIndex.java
+++ src/headfirst/designpatterns/observer/weatherobservable/WeatherStationHeatIndex.java
@@ -8,9 +8,12 @@ public class WeatherStationHeatIndex {
 		StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
 		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 		HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+		ArchiveDisplay archiveDisplay = new ArchiveDisplay(weatherData);
 
 		weatherData.setMeasurements(80, 65, 30.4f);
 		weatherData.setMeasurements(82, 70, 29.2f);
 		weatherData.setMeasurements(78, 90, 29.2f);
+
+		archiveDisplay.display();
 	}
 }
